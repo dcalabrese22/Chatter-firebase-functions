329a55e8a0f6edf05fa939951076a2ea2a371cb0
dcalabrese22 <dcalabrese22@gmail.com>
2017-11-20 21:48:01 -0500

    updated

--- functions/index.js
+++ functions/index.js
@@ -7,32 +7,34 @@ exports.sendNewMessageNotification = functions.database
     .ref('conversations/{userId}/{conversationId}')
     .onWrite(event => {
 
-        var userId = event.params.userId;
-        var conversationId = event.pararms.conversationId;
-        // console.log('event data:'.concat(event.data.val()));
-        // const conversation = event.data.val();
-        // const lastMessageType = conversation.lastMessageType;
-        // const sender = conversation.user1;
-        // const sr1 = 'New message from ';
-        // const notificationTitle = sr1.concat(sender);
-        // const notificationBody = conversation.lastMessage
-        //
-        // console.log('last message type is: '.concat(lastMessageType));
+        var userId = event.params.userId
+        var conversation = event.data.val();
+        const lastMessageType = conversation.lastMessageType;
+        const sender = conversation.user1;
+        const sr1 = 'New message from ';
+        const notificationTitle = sr1.concat(sender);
+        const notificationBody = conversation.lastMessage
 
-        // if (lastMessageType === "received") {
-        //     console.log("last message was received");
-        //     const deviceRefreshTokens = admin.database.ref('RefreshTokens/${userId}')
-        //         .once('value');
-        //
-        //     console.log('refresh tokens: '.concat(deviceRefreshTokens));
-        //
-        //     const payload = {
-        //         notification: {
-        //             title: notificationTitle,
-        //             body: notificationBody
-        //         }
-        //     }
-        //
-        //     return admin.messaging().sendToDevice(deviceRefreshTokens, payload);
-        // }
-});
+        console.log('last message type is: '.concat(lastMessageType));
+
+        if (lastMessageType === "received") {
+            console.log("last message was received");
+            return admin.database.ref('RefreshTokens/${userId}')
+                .once('value').then(snap => {
+
+                const deviceRefreshTokens = snap.val()
+                console.log('refresh tokens: '.concat(deviceRefreshTokens));
+            const payload = {
+                notification: {
+                    title: notificationTitle,
+                    body: notificationBody
+                }
+            }
+
+            return admin.messaging().sendToDevice(deviceRefreshTokens, payload).then(snap => {
+                return
+            });
+        })
+
+        }
+})
