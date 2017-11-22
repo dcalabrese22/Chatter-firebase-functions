9b4ef1b098edd20cabf0886949f9584964978e6b
dcalabrese22 <dcalabrese22@gmail.com>
2017-11-22 13:03:41 -0500

    created function to send message to user on new message received

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ admin.initializeApp(functions.config().firebase);
 
 exports.sendNewMessageNotification = functions.database
     .ref('conversations/{userId}/{conversationId}')
-    .onUpdate(event => {
+    .onWrite(event => {
         let userId = event.params.userId;
         let conversation = event.data.val();
         let lastMessageType = conversation.lastMessageType;
@@ -18,44 +18,19 @@ exports.sendNewMessageNotification = functions.database
                 title: notificationTitle,
                 body: notificationBody
             }
-        }
-        let dbRefString = 'RefreshTokens/'.concat(userId);
+        };
+        let tokenStrings = [];
+        let dbRefString = '/RefreshTokens/'.concat(userId);
+        console.log(dbRefString);
         if (lastMessageType === "received") {
-            const getTokenPromise = admin.database().ref(dbRefString).once('value');
-
-            return Promise.all([getTokenPromise])
+            return admin.database().ref(dbRefString).once('value', snapshot => {
+                snapshot.forEach(child => {
+                    tokenStrings.push(child.key);
+                    console.log(child.key);
+                    admin.messaging().sendToDevice(child.key, payload);
+                });
+            });
         } else {
             return 0;
         }
-
-
-        /*let userId = event.params.userId;
-
-        console.log('user id '.concat(userId));
-		console.log('title '.concat(notificationTitle));
-		console.log('body '.concat(notificationBody));
-
-        if (lastMessageType === "received") {
-			let deviceTokenPromise = admin.database().ref('RefreshTokens/'.concat(userId)).once('value');
-			return Promise.all([deviceTokenPromise]).then(results => {
-				let tokenSnapshot = results[0];
-				let payload = {
-                  notification: {
-                      title: notificationTitle,
-                      body: notificationBody
-                  }
-				}
-				let tokens = Object.key(tokenSnapshot.val());
-				let myToken = 'd5LFr4EchpY:APA91bH82SpnkXgZVjsft60h1M37NcXFhyCqaW-KarJO6nGJMgK8FzCMLLAqJ30lRTc1tFehxhV-y5cQf5WS6LFRqS0TSWK0nDNm13UfTVoZhp4Qs1AEEa-VNQJeqBHe5d3Ng2W1-Q3X'
-				console.log(myToken);
-				admin.messaging().sendToDevice(myToken, payload).then(function (response) {
-					console.log('Successfully sent message: ', response);
-				})
-				.catch(function (error) {
-					console.log.('Error sending message: ', error);
-				});
-			});
-        } else {
-            return 0;
-        }*/
     });
