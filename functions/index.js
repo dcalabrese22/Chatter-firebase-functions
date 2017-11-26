e71db24f03077d236a87fdba8ed991d7db41e5c1
dcalabrese22 <dcalabrese22@gmail.com>
2017-11-26 18:18:22 -0500

    feat: created function to notify user on new message received

--- functions/index.js
+++ functions/index.js
@@ -16,38 +16,66 @@ exports.sendNewMessageNotification = functions.database
 
         let sr1 = 'New message from ';
         let notificationTitle = sr1.concat(sender);
-        let payload = {
-            notification: {
-                title: notificationTitle,
-                body: body,
-                id: conversationId
-            }
-        };
-        let tokensArray = [];
-        console.log(userId, payload);
-        let getTokensArray = admin.database().ref('RefreshTokens').child(userId).once('value').then(snapshot => {
-            snapshot.forEach(child => {
-                tokensArray.push(child.key);
-            });
-            return Promise.all(tokensArray);
+
+        return loadUserTokens(userId).then(tokens => {
+
+            let payload = {
+                data: {
+                    title: notificationTitle,
+                    body: body,
+                    id: conversationId,
+                    sound: 'default'
+                }
+            };
+
+            return admin.messaging().sendToDevice(tokens, payload).then(response => {
+                const tokensToRemove = [];
+                response.results.forEach((result, index) => {
+                    const error = result.error;
+                    if (error) {
+                        console.error('Failure sending notification to ', tokens[index], error);
+
+                        if (error.code === 'messaging/registration-token-not-registered' ||
+                            'messaging/invalid-registration-token') {
+                            tokensToRemove.push(admin.database().ref('RefreshTokens').child(userId).child(tokens[index]).remove());
+                        }
+                    }
+                });
+                // return admin.database().ref('Notifications').child(conversationId).child(pushId).remove();
+                return Promise.all(tokensToRemove);
+            })
+
         });
 
-        console.log(tokensArray);
-        return admin.messaging().sendToDevice(tokensArray, payload).then(response => {
-            response.results.forEach((result, index) => {
-                const error = result.error;
-                let tokensToRemove = [];
-                if (error) {
-                    console.error('Failure sending notification to ', tokensArray[index], error);
 
-                    if (error.code === 'messaging/invalid-registration-token' ||
-                    error.code === 'messaging/registration-token-not-registered') {
-                        tokensToRemove.push(admin.database().ref('RefreshToken').child(userId)
-                            .child(tokensArray[index]).remove());
-                    }
-                }
-                return Promise.all(tokensToRemove)
-            });
-        })
+        // let getDeviceTokensPromise = admin.database().ref(`RefreshTokens`).child(userId).once('value');
+        // let getGarbasePromse = admin.database().ref(`users`).child(userId).once('value');
+        //
+        // return Promise.all([getDeviceTokensPromise, getGarbasePromse]).then(results => {
+        //     const tokensSnapshot = results[0];
+        //     const userSnapshot = results[1];
+        //
+        //     console.log('There are', tokensSnapshot.numChildren(), 'tokens to send notifications to.');
+        //
 
+        //
+        //     const tokens = Object.keys(tokensSnapshot.val());
+        //
+
+        // });
     });
+
+function loadUserTokens(userId) {
+    let dbRef = admin.database().ref('RefreshTokens').child(userId);
+    let defer = new Promise((resolve, reject) => {
+        dbRef.once('value', (snap) => {
+            let data = snap.val();
+            let tokens = Object.keys(data);
+            console.log(tokens);
+            resolve(tokens);
+        }, (err) => {
+            reject(err);
+        });
+    });
+    return defer;
+}
